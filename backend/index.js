1200a839163db7bd81a123783c8a5f79949b5cbd
Siddharth Jain <82303473+siddharthjain2199@users.noreply.github.com>
2023-01-14 23:08:01 +0530

    Final

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const connectToMongo = require('./db')
+const path = require("path")
 const express = require('express')
-var cors = require('cors')
+var cors = require('cors');
 
 connectToMongo();
 const app = express()
-const port = 5000 //3000
-
+const port = process.env.port || 5000 //3000
  
 app.use(cors())
 app.use(express.json())
@@ -16,6 +16,21 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+app.use(express.static(path.join(__dirname, "./build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
 app.listen(port, () => {
-  console.log(`iNotebook backend listening on port http://localhost:${port}`)
+  // try {
+  //   connectToMongo();
+    console.log(`iNotebook backend listening on port http://localhost:${port}`)    
+  // } catch (e) {
+  //   console.log(e.message)
+  // }
 })
